23054cb3ed28444c273a9b84cd8f828045673f96
Rick Osowski <rosowski@gmail.com>
2022-04-13 13:21:46 -0500

    SQUASH - TEST - Use pod shortname for Machine ID
    Signed-off-by: Rick Osowski <rosowski@gmail.com>

--- k8_cortx_cloud/deploy-cortx-cloud.sh
+++ k8_cortx_cloud/deploy-cortx-cloud.sh
@@ -747,11 +747,11 @@ function deployCortxConfigMap()
         # Build out FQDN of server pods 
         local pod_name="${server_pod_prefix}-${count}"
         local pod_fqdn="${pod_name}.${cortxserver_service_headless_name}.${namespace}.svc.${cluster_domain}"
+        local md5hash=$(echo -n "${pod_name}" | md5sum | awk '{print $1}')
         helm_install_args+=(
             ### TODO CORTX-28968 Does ${pod_name} below need to be ${pod_fqdn} for the Helm chart instead?
             ### Reviewing internals of cortx-configmap Helm Chart would say "no".
             ##--set "clusterStorageSets.${storage_set_name}.nodes.${pod_name}.serverUuid=${pod_fqdn}"
-            md5hash=$(echo -n "${pod_name}" | md5sum | awk '{print $1}')
             --set "clusterStorageSets.${storage_set_name}.nodes.${pod_name}.serverUuid=${md5hash}"
             --set "cortxMotr.rgwEndpoints[${count}]=tcp://${pod_fqdn}:21001"
             --set "cortxHare.haxServerEndpoints[${count}]=tcp://${pod_fqdn}:22001"
